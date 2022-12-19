bc40debd1672cb92c44c45dca46764d1a7dbe364
Namazbek Bekzhanov <namazbekzhan@gmail.com>
2022-12-20 00:21:04 +0600

    I added Marketplace

--- mainPageJs.js
+++ mainPageJs.js
@@ -79,3 +79,26 @@ subs.onclick= function() {
   subs.style.backgroundColor = "green";
   subs.innerHTML = 'Done!';
 }
+
+var images = [
+  'C:\Users\а\Desktop\finImages\eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMTEyNjcvaW1hZ2UvMTY2ODAzNjExMi9jaG9wLXRoZW0tdm9jYWxzLWktdm9jYWwtY2hvcC1iZWF0cy5wbmciLCJ0aW1lc3RhbXAiOm51bGwsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJmaWxsIiwid2lkdGgiOj.png',
+  'C:\Users\а\Desktop\finImages\eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMTEyNjcvaW1hZ2UvMTY3MDM1NDQyMy93b3JsZC1jdXAtb2YtYmVhdHMtdGV4dC1sb2dvLS5qcGciLCJ0aW1lc3RhbXAiOm51bGwsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJmaWxsIiwid2lkdGgiOjQwMCwiaG.png',
+  'C:\Users\а\Desktop\finImages\eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMTEyNjcvaW1hZ2UvMTY2OTQ0MDk3NC9zdG9yeXRlbGxpbmctYmVhdHMtY292ZXIucG5nIiwidGltZXN0YW1wIjpudWxsLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiZmlsbCIsIndpZHRoIjo0MDAsImhlaWdodC.png',
+  'C:\Users\а\Desktop\finImages\eyJidWNrZXQiOiJidHMtY29udGVudCIsImtleSI6InVzZXJzL3Byb2QvMTEyNjcvaW1hZ2UvUmlKVnFFQjl3ZmQ3L2p1bGNvdmVyLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJmaWxsIiwid2lkdGgiOjQwMCwiaGVpZ2h0Ijo0MDB9fX0=.png',
+  'C:\Users\а\Desktop\finImages\eyJidWNrZXQiOiJwcm9kLWJ0cy1wbGF5bGlzdCIsImtleSI6InByb2QvcGxheWxpc3QvYXJ0d29yay9QTDUwMDU5NTgvYXJ0d29yay5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiZmlsbCIsIndpZHRoIjo0MDAsImhlaWdodCI6NDAwfX19.png'
+];
+var currentpic = 0;
+var lastpic = images.length-1;
+function nextslide()
+{
+    if (currentpic == lastpic)
+    {
+        currentpic = 0;
+        document.getElementById('slide').src = images[currentpic];
+    }
+    else
+    {
+        currentpic++;
+        document.getElementById('slide').src = images[currentpic];
+    }
+}
